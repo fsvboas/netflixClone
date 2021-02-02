ab07855834170e0c8caaf0430b0f05660b77833a
Felippe Vilas Boas <75647162+fsvboas@users.noreply.github.com>
2021-02-02 02:00:49 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    scroll: true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
